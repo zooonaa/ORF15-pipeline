b6a781f15320a421d11365751cf80b63de6ef768
Chien-Yu Lin <145662771+zooonaa@users.noreply.github.com>
2025-08-24 13:39:08 -0400

    Update sh_all.py

--- sh_all.py
+++ sh_all.py
@@ -2,124 +2,125 @@ import os, sys
 import json
 
 #=================================basic========================================#
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
-os.system('mkdir '+'run/')
-os.system('cd run/')
-path1 = os.path.join(script_dir, "run/")
+work_dir   = os.path.join(script_dir, "run")
+os.makedirs(work_dir, exist_ok=True)  
+# os.chdir(work_dir)
+
 SampleList = os.path.join(script_dir, "list.txt")
-fastq_path = os.path.join(script_dir, "list.txt")
 
+fastq_dir = os.path.join(script_dir, "fastq_trimmed")  
+fastq_trim_d = os.path.join(fastq_dir, "d_single_test100")
 
-fastq_trim_d=fastq_path+"d_single_test100"
+data = "_PCR"
+tool = "_all"
+align = "_bwamem"
+threading = "16"
 
-data="_PCR"
-tool = '_all'
-align = '_bwamem'
-threading ='16'
 #=======================software direction=====================================#
 with open(os.path.join(script_dir, "config.json")) as f:
     config = json.load(f)
 
-bwa = os.path.join(config["bwa"])
-picard = os.path.join(config["picard"])
-GATK4 = os.path.join(config["GATK4"])
-samtools = os.path.join(config["samtools"])
-ref_ucsc = os.path.join(config["ref_ucsc"])
-ref_fai = os.path.join(config["ref_fai"])
-samclip = os.path.join(config["samclip"])
+bwa       = os.path.join(config["bwa"])
+picard    = os.path.join(config["picard"])
+GATK4     = os.path.join(config["GATK4"])
+samtools  = os.path.join(config["samtools"])
+ref_ucsc  = os.path.join(config["ref_ucsc"])
+ref_fai   = os.path.join(config["ref_fai"])
+samclip   = os.path.join(config["samclip"])
 
-#sample_list = os.path.join(script_dir, config["SampleList"])
-#fastq_dir = os.path.join(script_dir, config["fastq_dir"])
-#output_dir = os.path.join(script_dir, config["output_dir"])
+jar = 'java -Xmx80g -jar'  # 40g/80g
+python3 = "/bin/python3.6"
 
-jar = 'java  -Xmx80g -jar'; #40g/80g (V4_VERSION=80)
-python3="/bin/python3.6"
 #=============================================================#
 
-
 with open(SampleList, 'r') as file:
-    S_List = file.readlines()
-S_List = [i.strip() for i in S_List]
+    S_List = [line.strip() for line in file]
+
 node_range = range(1, 101)
-for i in S_List:
-    sample_dir = os.path.join(path1, i)
+
+for sample in S_List:
+    sample_dir = os.path.join(work_dir, sample)
     os.makedirs(sample_dir, exist_ok=True)
+
     for node in node_range:
         status = f"s{node}"
         status_dir = os.path.join(sample_dir, status)
         os.makedirs(status_dir, exist_ok=True)
-        fastq_r1 = f"/{fastq_trim_d}/{i}-PCR_merge_qc_{status}_100000.fastq"
-        os.system('mkdir'+' '+i)
-        file1 = open(i+'_'+status+'.sh', 'w', encoding='utf-8')
-#========================================================#
 
-        file1.write('#!/usr/bin/sh'+'\n')
-        file1.write('#SBATCH -A MST109178'+'\n')        # Account name/project number				（科技部計畫編號！，要去國網上面查詢）
-        file1.write('#SBATCH -J '+i+'_RPGR'+'\n')         # Job name		 				（可以自己隨便改）
-        file1.write('#SBATCH -p ngs186G'+'\n')           # Partition Name 等同PBS裡面的 -q Queue name  			（除非WES,WGS才會去改，寫法可以去國網上PARTITION,,看）
-        file1.write('#SBATCH -c 28'+'\n')               # 使用的core數 請參考Queue資源設定 				（跟partition一起）
-        file1.write('#SBATCH --mem=186g'+'\n')          # 使用的記憶體量 請參考Queue資源設定				（跟partition一起）
-        file1.write('#SBATCH -o '+i+'_'+status+'_'+'out.log'+'\n')          # Path to the standard output file 				（也可以絕對路徑/out.log）
-        file1.write('#SBATCH -e '+i+'_'+status+'_'+'err.log'+'\n')         # Path to the standard error ouput file			（同上）
-        file1.write('#SBATCH --mail-user=zona890721@gmail.com'+'\n')    # email					（改成自己的email）
-        file1.write('#SBATCH --mail-type=FAIL,END'+'\n')  
-    
+        fastq_r1 = os.path.join(fastq_trim_d, f"{sample}-PCR_merge_qc_{status}_100000.fastq")
 
-    ### bwa_mem ###
-        file1.write(bwa+' mem -t '+threading+' -T 100 -R '+'\'@RG\\tID:'+i+data+align+'\\tLB:'+i+data+align+'\\tSM:'+i+data+align+'\\tPL:ILLUMINA\\\' '+
-                        ref_ucsc+' '+fastq_r1+' > '+path1+i+'/'+status+'/'+i+data+align+'.sam'+' '+'&&'+'\n' )
-        file1.write('\n' )
+        sh_path = os.path.join(sample_dir, f"{sample}_{status}.sh")
+        with open(sh_path, 'w', encoding='utf-8') as file1:
+            file1.write('#!/usr/bin/sh\n')
 
+            sam_path        = os.path.join(status_dir, f"{sample}{data}{align}.sam")
+            samclip_path    = os.path.join(status_dir, f"{sample}{data}{align}_samclip_.sam")
+            bam_path        = os.path.join(status_dir, f"{sample}{data}{align}.bam")
+            bam_fq_path     = os.path.join(status_dir, f"{sample}{data}{align}_fq.bam")
+            bam_marked_path = os.path.join(status_dir, f"{sample}{data}{align}.marked.bam")
+            bam_sorted_idx  = os.path.join(status_dir, f"{sample}{data}{align}.marked.indexed.bam")
+            hc_vcf          = os.path.join(status_dir, f"{sample}{data}{align}.haplotype_region.SnpIndel.vcf.gz")
+            hc_bamout       = os.path.join(status_dir, f"{sample}{data}{align}.haplotype_region.bamout.bam")
+            mut_vcf         = os.path.join(status_dir, f"{sample}{data}{align}.Mutect2_region.vcf.gz")
+            mut_bamout      = os.path.join(status_dir, f"{sample}{data}{align}.Mutect2_region.bamout.bam")
+            metrics_file    = os.path.join(status_dir, f"{sample}{data}{align}_metrics")
 
-    ### Samclip ###
+            # bwa mem
+            file1.write(
+                f"{bwa} mem -t {threading} -T 100 -R "
+                f"'@RG\\tID:{sample}{data}{align}\\tLB:{sample}{data}{align}\\tSM:{sample}{data}{align}\\tPL:ILLUMINA' "
+                f"{ref_ucsc} {fastq_r1} > {sam_path} &&\n\n"
+            )
 
-        file1.write(samclip+' --ref '+ref_fai+' < '+path1+i+'/'+status+'/'+i+data+align+'.sam'+' > '+path1+i+'/'+status+'/'+i+data+align+'_samclip_.sam'+' &&'+'\n')
-        file1.write('\n' )
+            # samclip
+            file1.write(f"{samclip} --ref {ref_fai} < {sam_path} > {samclip_path} &&\n\n")
 
+            # picard SortSam
+            file1.write(
+                f"{jar} {picard} SortSam INPUT={samclip_path} OUTPUT={bam_path} "
+                f"SORT_ORDER=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true &&\n\n"
+            )
 
-    ### picard_SortSam ### 
+            # samtools filter & index
+            file1.write(f"{samtools} view -h -q 60 -b {bam_path} > {bam_fq_path} &&\n\n")
+            file1.write(f"{samtools} index {bam_fq_path} &&\n\n")
 
-        file1.write(jar+' '+picard+' '+'SortSam INPUT='+path1+i+'/'+status+'/'+i+data+align+'_samclip_.sam'+' '+'OUTPUT='+path1+i+'/'+status+'/'+i+data+align+'.bam'+
-                      ' '+'SORT_ORDER=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'+' '+'&&'+'\n') 
-        file1.write('\n' )
+            # picard MarkDuplicates
+            file1.write(
+                f"{jar} {picard} MarkDuplicates INPUT={bam_fq_path} OUTPUT={bam_marked_path} "
+                f"METRICS_FILE={metrics_file} VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true &&\n\n"
+            )
 
+            # picard SortSam (again)
+            file1.write(
+                f"{jar} {picard} SortSam INPUT={bam_marked_path} OUTPUT={bam_sorted_idx} "
+                f"SORT_ORDER=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true &&\n\n"
+            )
 
-    ### samtools!
+            # GATK HaplotypeCaller
+            file1.write(
+                f"{jar} {GATK4} HaplotypeCaller --minimum-mapping-quality 60 -L X:38145300-38145800 "
+                f"-R {ref_ucsc} -I {bam_sorted_idx} -O {hc_vcf} -bamout {hc_bamout}\n\n"
+            )
 
+            # GATK Mutect2
+            file1.write(
+                f"{jar} {GATK4} Mutect2 --minimum-mapping-quality 60 -L X:38145300-38145800 "
+                f"-R {ref_ucsc} -I {bam_sorted_idx} -O {mut_vcf} -bamout {mut_bamout}\n\n"
+            )
 
-        file1.write(samtools+' view -h -q 60 -b '+path1+i+'/'+status+'/'+i+data+align+'.bam'+' > '+path1+i+'/'+status+'/'+i+data+align+'_fq.bam'+' '+'&&'+'\n')
-        file1.write('\n' )
+            file1.write(f"rm {sam_path}\n")
+            file1.write(f"rm {samclip_path}\n")
+            file1.write(f"rm {bam_path}\n")
+            file1.write(f"rm {bam_fq_path}\n")
 
-        file1.write(samtools+' index '+path1+i+'/'+status+'/'+i+data+align+'_fq.bam'+' '+'&&'+'\n')
-        file1.write('\n' )
+import time
+import glob
 
+sh_files = sorted(glob.glob(os.path.join(work_dir, "*/*.sh")))
 
-    ### picard_MarkDuplicates ###
-        file1.write(jar+' '+picard+' '+'MarkDuplicates'+' '+'INPUT='+path1+i+'/'+status+'/'+i+data+align+'_fq.bam'+' '+'OUTPUT='+path1+i+'/'+status+'/'+i+data+align+'.marked.bam'+' '+'METRICS_FILE='+path1+i+'/'+status+'/'+i+data+align+'_metrics'+' '+'VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'+' '+'&&'+'\n')
-        file1.write('\n' )
-
-
-    ### sh09_picard_SortSam ###
-        file1.write(jar+' '+picard+' '+'SortSam'+' '+'INPUT='+path1+i+'/'+status+'/'+i+data+align+'.marked.bam'+' '+'OUTPUT='+path1+i+'/'+status+'/'+i+data+align+'.marked.indexed.bam'+' '+'SORT_ORDER=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'+' '+'&&'+'\n')
-        file1.write('\n' )
-        
-
-    ### GATK_HaplotypeCaller ###
-        file1.write(jar+' '+GATK4+' '+'HaplotypeCaller --minimum-mapping-quality 60 -L X:38145300-38145800 -R'+' '+ref_ucsc+' '+'-I'+' '+path1+i+'/'+status+'/'+i+data+align+'.marked.indexed.bam'+' '+'-O'+' '+path1+i+'/'+status+'/'+i+data+align+'.haplotype_region.SnpIndel.vcf.gz'+' '+'-bamout '+path1+i+'/'+status+'/'+i+data+align+'.haplotype_region.bamout.bam'+'\n')
-        file1.write('\n' )
-
-
-
-    ### GATK_Mutect2 ###
-        file1.write(jar+' '+GATK4+' '+'Mutect2 --minimum-mapping-quality 60 -L X:38145300-38145800 -R'+' '+ref_ucsc+' '+'-I'+' '+path1+i+'/'+status+'/'+i+data+align+'.marked.indexed.bam'+' '+'-O'+' '+path1+i+'/'+status+'/'+i+data+align+'.Mutect2_region.vcf.gz'+' '+'-bamout '+path1+i+'/'+status+'/'+i+data+align+'.Mutect2_region.bamout.bam'+'\n')
-        file1.write('\n' )
-
-        file1.write('rm '+path1+i+'/'+status+'/'+i+data+align+'.sam'+'\n')
-        file1.write('rm '+path1+i+'/'+status+'/'+i+data+align+'_samclip_.sam'+'\n')
-        file1.write('rm '+path1+i+'/'+status+'/'+i+data+align+'.bam'+'\n')
-        file1.write('rm '+path1+i+'/'+status+'/'+i+data+align+'_fq.bam'+'\n')
-
-        file1.close()
-#        os.system('sbatch '+i+'_'+status+'.sh')
-    
+for sh in sh_files:
+    print(f"Submitting {sh} ...")
+    os.system(f"sbatch {sh}")
+    time.sleep(0.8) 
